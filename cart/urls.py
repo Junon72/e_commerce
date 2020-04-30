7dd7473c25131b84b30cf61778d28102f92926bd
Jussi Nousiainen <juno.athome@gmail.com>
2020-04-30 19:11:34 +0200

    corrected folder name and reverse path of add_to_card

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
 	path('', views.view_cart, name="view_cart"),
-	path('add/<int:id>', views.add_to_cart, name="add_to_cart"),
-	path('adjust/<int:id>/', views.adjust_cart, name="adjust_cart")
+	path('add/<id>', views.add_to_cart, name="add_to_cart"),
+	path('adjust/<id>/', views.adjust_cart, name="adjust_cart")
 ]
